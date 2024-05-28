12fb2a02a3e1b4c978e5b83d7beaa377da947cba
Kelvin Creighton <kelvincreighton413@gmail.com>
2024-05-28 10:11:27 -0600

    added collisio

--- OilSpill.py
+++ OilSpill.py
@@ -58,18 +58,74 @@ def moveProjectiles(vx=0, vy=100):
         x = projectile.xcor() + vx
         y = projectile.ycor() + vy
         if x < -screenWidth or x > screenWidth or y < -screenHeight or y > screenHeight:
-            deleteProjectile(projectile)
+            deleteSprite(projectile)
             continue
         projectile.setx(x)
         projectile.sety(y)
 
-def deleteProjectile(projectile):
-    projectile.hideturtle()
-    projectile_list.remove(projectile)
+def deleteSprite(sprite):
+    sprite.hideturtle()
+    # Try and remove the sprite from both lists
+    # This method sucks but the real solution is long and complicated
+    try:
+        enemy_list.remove(sprite)
+    except:
+        try:
+            projectile_list.remove(sprite)
+        except:
+            pass # ew, bad, kill it with fire !!! (sorry)
 
-def spriteCollide(s1, s2, diameter=100):
-    # a^2 + b^2 >= c^2 --> sqrt(a^2 + b^2) >= c
-    return (s1.xcor()-s2.xcor())**2 + (s1.ycor()-s2.ycor())**2 >= diameter**2
+def spriteCollide(spriteName1, spriteName2, diameter=100):
+    spriteName1 = spriteName1.lower()
+    spriteName2 = spriteName2.lower()
+    if spriteName1 == spriteName2:
+        print("A sprite cannot collide with itself")
+        print("===================================")
+        return None, None
+    if not (spriteName1 == "player" or spriteName1 == "enemy" or spriteName1 == "projectile"):
+        print("The FIRST sprite name does not match any existing sprite (player, enemy, projectile)")
+        print("====================================================================================")
+        return None, None
+    if not (spriteName2 == "player" or spriteName2 == "enemy" or spriteName2 == "projectile"):
+        print("The SECOND sprite name does not match any existing sprite (player, enemy, projectile)")
+        print("=====================================================================================")
+        return None, None
+
+    # Set up the turtles to always be arrays based on the different types
+    turtle1 = None
+    turtle2 = None
+    if spriteName1 == "player":
+        turtle1 = [ player ]
+    if spriteName2 == "player":
+        turtle2 = [ player ]
+    if spriteName1 == "enemy":
+        turtle1 = enemy_list
+    if spriteName2 == "enemy":
+        turtle2 = enemy_list
+    if spriteName1 == "projectile":
+        turtle1 = projectile_list
+    if spriteName2 == "projectile":
+        turtle2 = projectile_list
+
+
+    for t1 in turtle1:
+        for t2 in turtle2:
+            # a^2 + b^2 <= c^2 --> sqrt(a^2 + b^2) <= c
+            if (t1.xcor()-t2.xcor())**2 + (t1.ycor()-t2.ycor())**2 <= diameter**2:
+                return t1, t2   # Return which sprites collided
+    return None, None
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Define functions to control player movement
 def move_left():
@@ -95,7 +151,8 @@ turtle.onkey(shoot_projectile, "space")
 
 # Main game loop
 enemyCreateTimer = 0
-while True:
+playing_game = True
+while playing_game:
     enemyCreateTimer = enemyCreateTimer + 1
     if enemyCreateTimer >= 100:
         enemy_x = random.randrange(-screenWidth, screenWidth)
@@ -103,5 +160,19 @@ while True:
         addEnemy(enemy_x, enemy_y)
         enemyCreateTimer = 0
 
-    moveProjectiles(0, 100)
+    xspeed = 0
+    yspeed = 100
+    moveProjectiles(xspeed, yspeed)
+    projectile_sprite, enemy_sprite = spriteCollide("projectile", "enemy")
+    if projectile_sprite and enemy_sprite:
+        deleteSprite(enemy_sprite)
+        deleteSprite(projectile_sprite)
+
+    player_sprite, enemy_sprite = spriteCollide("player", "enemy")
+    if player_sprite and enemy_sprite:
+        print("====================================================================================")
+        print("                                Game Over")
+        print("====================================================================================")
+        playing_game = False
+
     screen.update()
