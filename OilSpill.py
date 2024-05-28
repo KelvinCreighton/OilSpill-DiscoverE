03f1f0ba49ebeea491dd81e621bf723da7bc179c
Kelvin Creighton <kelvincreighton413@gmail.com>
2024-05-28 11:50:40 -0600

    this lags too much i might scrap my version :(

--- OilSpill.py
+++ OilSpill.py
@@ -15,13 +15,14 @@ turtle.register_shape(enemyImage)
 turtle.register_shape(projectileImage)
 
 # Initialize the screen
-screenWidth = 1280//2
-screenHeight = 960//2
+screenWidth = 1024//2
+screenHeight = 760//2
 screen = turtle.Screen()
 screen.setup(width=screenWidth*2, height=screenHeight*2)
+screenWidth -= 50   # Adjust for sprite sizes to help with wall collision centering
 screen.title("Oil Spill Clean Up")
 screen.bgcolor("black")
-screen.bgpic("files/images/background.gif")
+screen.bgpic(backgroundImage)
 
 
 # Create the player
@@ -32,14 +33,36 @@ player.speed(0)
 player.setposition(0, -screenHeight/2)
 
 # Create the enemies
+global enemy_list_index, enemy_list_len
+enemy_list_index = 0
+enemy_list_len = 4
 enemy_list = []
-def addEnemy(x, y):
+enemy_vx_direction_list = []    # 1 for normal movement, -1 for reverse movement after wall bounce
+for i in range(enemy_list_len):
     enemy = turtle.Turtle()
-    enemy.speed(0)
     enemy.penup()
-    enemy.setposition(x, y)
+    enemy.hideturtle()
     enemy.shape(enemyImage)
     enemy_list.append(enemy)
+    enemy_vx_direction_list.append(1)
+
+def addEnemy(x, y):
+    global enemy_list_index, enemy_list_len
+    if enemy_list_index >= enemy_list_len:
+        return
+    enemy_list[enemy_list_index].setposition(x, y)
+    enemy_list[enemy_list_index].showturtle()
+    enemy_list_index += 1
+
+def moveEnemies(vx, vy, bounce_on_walls=False):
+    for i in range(len(enemy_list)):
+        x = enemy_list[i].xcor() + vx*enemy_vx_direction_list[i]
+        y = enemy_list[i].ycor() + vy
+        if bounce_on_walls and (x <= -screenWidth or x >= screenWidth):
+            enemy_vx_direction_list[i] *= -1
+        enemy_list[i].setx(x)
+        enemy_list[i].sety(y)
+
 
 # Create the projectiles
 projectile_list = []
@@ -53,7 +76,7 @@ def addProjectile():
     projectile.shape(projectileImage)
     projectile_list.append(projectile)
 
-def moveProjectiles(vx=0, vy=100):
+def moveProjectiles(vx=0, vy=100, bounce_on_walls=False):
     for projectile in projectile_list:
         x = projectile.xcor() + vx
         y = projectile.ycor() + vy
@@ -81,38 +104,25 @@ def spriteCollide(spriteName1, spriteName2, diameter=100):
     if spriteName1 == spriteName2:
         print("A sprite cannot collide with itself")
         print("===================================")
-        return None, None
-    if not (spriteName1 == "player" or spriteName1 == "enemy" or spriteName1 == "projectile"):
-        print("The FIRST sprite name does not match any existing sprite (player, enemy, projectile)")
+
+    def getSpriteList(name):
+        if name == "player":
+            return [player]
+        elif name == "enemy":
+            return enemy_list
+        elif name == "projectile":
+            return projectile_list
+        print("One of the sprite name do not match any existing sprite (player, enemy, projectile)")
         print("====================================================================================")
-        return None, None
-    if not (spriteName2 == "player" or spriteName2 == "enemy" or spriteName2 == "projectile"):
-        print("The SECOND sprite name does not match any existing sprite (player, enemy, projectile)")
-        print("=====================================================================================")
-        return None, None
+        return []
 
-    # Set up the turtles to always be arrays based on the different types
-    turtle1 = None
-    turtle2 = None
-    if spriteName1 == "player":
-        turtle1 = [ player ]
-    if spriteName2 == "player":
-        turtle2 = [ player ]
-    if spriteName1 == "enemy":
-        turtle1 = enemy_list
-    if spriteName2 == "enemy":
-        turtle2 = enemy_list
-    if spriteName1 == "projectile":
-        turtle1 = projectile_list
-    if spriteName2 == "projectile":
-        turtle2 = projectile_list
+    sprite1 = getSpriteList(spriteName1)
+    sprite2 = getSpriteList(spriteName2)
 
-
-    for t1 in turtle1:
-        for t2 in turtle2:
-            # a^2 + b^2 <= c^2 --> sqrt(a^2 + b^2) <= c
-            if (t1.xcor()-t2.xcor())**2 + (t1.ycor()-t2.ycor())**2 <= diameter**2:
-                return t1, t2   # Return which sprites collided
+    for s1 in sprite1:
+        for s2 in sprite2:
+            if (s1.xcor() - s2.xcor())**2 + (s1.ycor() - s2.ycor())**2 <= diameter**2:
+                return s1, s2
     return None, None
 
 
@@ -153,16 +163,23 @@ turtle.onkey(shoot_projectile, "space")
 enemyCreateTimer = 0
 playing_game = True
 while playing_game:
+    # Creating enemies on a timer
     enemyCreateTimer = enemyCreateTimer + 1
-    if enemyCreateTimer >= 100:
+    if enemyCreateTimer >= 2:
         enemy_x = random.randrange(-screenWidth, screenWidth)
         enemy_y = screenHeight
         addEnemy(enemy_x, enemy_y)
         enemyCreateTimer = 0
 
+    # Moving the projectiles and the enemies
     xspeed = 0
     yspeed = 100
     moveProjectiles(xspeed, yspeed)
+    xspeed = 60
+    yspeed = -20
+    bounce_on_walls = True
+    moveEnemies(xspeed, yspeed, bounce_on_walls)
+
     projectile_sprite, enemy_sprite = spriteCollide("projectile", "enemy")
     if projectile_sprite and enemy_sprite:
         deleteSprite(enemy_sprite)
